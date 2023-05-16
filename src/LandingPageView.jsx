6a3dde970dc909b65a8f27bb8c005b529fe84e5a
Daniel Schroter <daniel@schroter.biz>
2023-05-16 09:09:59 +0200

    Redesign Bubbles and Card Hover effect

--- src/LandingPageView.jsx
+++ src/LandingPageView.jsx
@@ -11,13 +11,25 @@ import Footer from "./components/footer/Footer";
 function LandingPageView() {
   return (
     <>
-      <Header />
-      <Nav />
-      <About />
-      <Experience />
-      <Portfolio />
-      <Contact />
-      <Footer />
+      <div className="relative inset-0 overflow-hidden ">
+        {/* <div className="absolute top-0 w-72 h-72 bg-gray-300 rounded-full left-[-10vw] filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay"></div> */}
+        <div className="absolute top-[120vh] rounded-full w-72 h-72 bg-slate-200 left-[10vw] mix-blend-screen filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div>
+        <div className="absolute top-7 rounded-full w-96 h-96 bg-slate-200 left-[30vw] mix-blend-screen filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-4000"></div>
+        <div className="absolute top-10 rounded-full w-96 h-96 bg-yellow-600/60 left-[40vw] mix-blend-screen  filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div>
+        {/* <div className="absolute top-2/4 rounded-full w-96 h-96 bg-yellow-600/60 left-[40vw] mix-blend-screen  filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div> */}
+        <div className="absolute top-[200vh] rounded-full w-96 h-96 bg-yellow-600/60 left-[40vw] mix-blend-screen  filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div>
+        <div className="absolute top-3/4 rounded-full w-96 h-96 bg-slate-200 left-[10vw] mix-blend-screen filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-4000"></div>
+        <div className="absolute top-1/4 rounded-full w-96 h-96 bg-slate-200 left-[20vw] mix-blend-screen filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div>
+        <div className="absolute top-2/4 rounded-full w-96 h-96 bg-slate-200/60 left-[40vw] mix-blend-screen  filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div>
+
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Portfolio />
+        <Contact />
+        <Footer />
+      </div>
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 // import ME from "../../assets/me-5.png";
-import ME from "../../assets/Mein Projekt (1).png";
+import ME from "../../assets/me-bewerbung.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
-      <div className="absolute inset-0 flex justify-center top-[-60px]">
+      {/* <div className="absolute inset-0 flex justify-center top-[-60px]">
         <div className="relative top-0 w-72 h-72 bg-gray-300 rounded-full left-50 filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay"></div>
         <div className="relative top-40 rounded-full w-96 h-96 bg-slate-200 right-40 filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div>
-      </div>
+      </div> */}
       <div className="container header__container">
         <h5 className="p-2">Hello I'm</h5>
         <h1 className=" pb-4 text-4xl md:text-4xl tracking-widest font-thin ">
@@ -23,7 +23,7 @@ const Header = () => {
           <div>{/* <HeaderSocials /> */}</div>
 
           <div className="me">
-            <img className="me-img" src={ME} alt="me" />
+            {/* <img className="me-img" src={ME} alt="me" /> */}
           </div>
           <div className="scroll__down-div">
             {/* <a href="#contact" className="scroll__down">
