f8d2cd12903e913d64e18e3a06d0b8f3ccf31d6b
Daniel Schroter <daniel@schroter.biz>
2023-05-16 16:19:47 +0200

    BUGFIX: IOS mix-blend-mode & image cuts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,10 +8,6 @@ import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
-      {/* <div className="absolute inset-0 flex justify-center top-[-60px]">
-        <div className="relative top-0 w-72 h-72 bg-gray-300 rounded-full left-50 filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay"></div>
-        <div className="relative top-40 rounded-full w-96 h-96 bg-slate-200 right-40 filter blur-3xl opacity-20 sm:opacity-20 animate-blob animation-delay-2000"></div>
-      </div> */}
       <div className="container header__container">
         <h5 className="p-2">Hello I'm</h5>
         <h1 className=" pb-4 text-4xl md:text-4xl tracking-widest font-thin ">
@@ -23,6 +19,7 @@ const Header = () => {
           <div>{/* <HeaderSocials /> */}</div>
 
           <div className="me">
+            {/* <div className="me-img"></div> */}
             {/* <img className="me-img" src={ME} alt="me" /> */}
           </div>
           <div className="scroll__down-div">
